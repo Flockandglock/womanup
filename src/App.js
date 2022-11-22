26f129ba1e0c0d0161c48192a0b047ced9df8ca8
Denis Starukhin <oggyqwe@gmail.com>
2022-11-22 13:02:08 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {TodoContext} from './context/index';
+
 import './App.css';
 
+import TaskHeader from './components/TaskHeader/TaskHeader';
+import TasksList from './components/TasksList/TasksList';
+import EditTask from './components/EditTask/EditTask';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const tasksArr = [
+        {id: 1, complited: false, title: 'Create tz', descr: 'some text', date: '12-12-2022', file: ''},
+        {id: 2, complited: true, title: 'Get item', descr: 'fetching item', date: '02-12-2022', file: ''},
+        {id: 3, complited: false, title: 'Come to home', descr: 'go throu to the forest', date: '14-04-2023', file: ''}
+    ];
+
+    const [title, setTitle] = useState('');
+    const [descr, setDescr] = useState('');
+    const [date, setDate] = useState('');
+    const [file, setFile] = useState('');
+
+    const [selectTask, setSelectTask] = useState(null);
+    const [edit, setEdit] = useState(false);
+    
+    const [tasks, setTasks] = useState(tasksArr);
+
+
+    return (
+    <TodoContext.Provider value={{title,
+                                descr,
+                                date,
+                                file,
+                                setTitle,
+                                setDescr,
+                                setDate,
+                                setFile,
+                                tasks,
+                                setTasks,
+                                selectTask,
+                                setSelectTask,
+                                setEdit,
+                                }}>
+        <div className="App">
+            <div>
+                <TaskHeader/>
+                <TasksList/>
+            </div>
+            {edit ? <EditTask/> : undefined}
+        </div>
+    </TodoContext.Provider>
+    );
 }
 
 export default App;
